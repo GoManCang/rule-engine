683f278975674a383c78244718d81f7d157a539f
zhengby <zhengby@DST61666.cn1.global.ctrip.com>
2015-06-04 09:44:50 +0800

    userprofile 依赖

--- ruleengine-bundle/src/main/java/com/ctrip/infosec/rule/resource/RiskProfileClient.java
+++ ruleengine-bundle/src/main/java/com/ctrip/infosec/rule/resource/RiskProfileClient.java
@@ -1,23 +1,23 @@
 package com.ctrip.infosec.rule.resource;
 
 import com.ctrip.infosec.sars.util.GlobalConfig;
-import com.ctrip.sec.userprofile.client.service.PassportService;
+import com.ctrip.sec.userprofile.client.service.RiskProfileClientConfig;
 import org.apache.commons.lang3.Validate;
 
 /**
  * Created by lpxie on 15-6-3.
  */
-public class RiskProfileClient
-{
+public class RiskProfileClient {
+
     static final String urlPrefix = GlobalConfig.getString("DataProxy.REST.URL.Prefix");
 
-    static void check(){
+    static void check() {
         Validate.notEmpty(urlPrefix, "在GlobalConfig.properties里没有找到\"DataProxy.REST.URL.Prefix\"配置项.");
     }
 
-    static void init()
-    {
+    static void init() {
         check();
-        PassportService.init(urlPrefix);
+        RiskProfileClientConfig client = new RiskProfileClientConfig();
+        client.init(urlPrefix);
     }
 }
