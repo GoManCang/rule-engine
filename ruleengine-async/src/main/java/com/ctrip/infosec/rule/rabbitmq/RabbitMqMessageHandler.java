f93d8741525b99476626766f39cb5ff1e13d308e
vjyx蒋一新 <yxjiang@ctrip.com>
2015-06-26 10:46:54 +0800

    修改bug：InternalRiskFact为空时抛出空指针异常

--- ruleengine-async/src/main/java/com/ctrip/infosec/rule/rabbitmq/RabbitMqMessageHandler.java
+++ ruleengine-async/src/main/java/com/ctrip/infosec/rule/rabbitmq/RabbitMqMessageHandler.java
@@ -20,7 +20,6 @@ import com.ctrip.infosec.rule.convert.internal.InternalRiskFact;
 import com.ctrip.infosec.rule.convert.persist.RiskFactPersistManager;
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.collections.MapUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -109,17 +108,19 @@ public class RabbitMqMessageHandler {
             //riskfact 数据映射转换
             internalRiskFact = riskFactConvertRuleService.apply(fact);
             // 数据落地
-            String operation = (internalRiskFact == null ? "SKIP" : internalRiskFact.getEventId()) + ".persist-info";
-            try {
-                beforeInvoke(operation);
-                RiskFactPersistManager persistManager = RiskFactPersistStrategy.preparePersistence(internalRiskFact);
-                persistManager.persist();
-                internalRiskFact.setReqId(persistManager.getGeneratedReqId());
-            } catch (Exception ex) {
-                fault(operation);
-                logger.error(Contexts.getLogPrefix() + "fail to persist risk fact.", ex);
-            } finally {
-                afterInvoke(operation);
+            if (internalRiskFact != null) {
+                String operation = internalRiskFact.getEventId() + ".persist-info";
+                try {
+                    beforeInvoke(operation);
+                    RiskFactPersistManager persistManager = RiskFactPersistStrategy.preparePersistence(internalRiskFact);
+                    persistManager.persist();
+                    internalRiskFact.setReqId(persistManager.getGeneratedReqId());
+                } catch (Exception ex) {
+                    fault(operation);
+                    logger.error(Contexts.getLogPrefix() + "fail to persist risk fact.", ex);
+                } finally {
+                    afterInvoke(operation);
+                }
             }
         } catch (Throwable ex) {
             logger.error(Contexts.getLogPrefix() + "invoke query exception.", ex);
@@ -154,15 +155,17 @@ public class RabbitMqMessageHandler {
                     }
 
                     // 发送Offline4J
-                    try {
-                    	Object eventObj = riskEventConvertor.convert(internalRiskFact, riskLevel, HeaderMappingBizType.Offline4J);
-                        beforeInvoke("offlineMessageSender.sendToOffline");
-                        offlineMessageSender.sendToOffline(eventObj);
-                    } catch (Exception ex) {
-                        fault("offlineMessageSender.sendToOffline");
-                        logger.error(Contexts.getLogPrefix() + "send Offline4J message fault.", ex);
-                    } finally {
-                        afterInvoke("offlineMessageSender.sendToOffline");
+                    if (internalRiskFact != null) {
+                        try {
+                            Object eventObj = riskEventConvertor.convert(internalRiskFact, riskLevel, HeaderMappingBizType.Offline4J);
+                            beforeInvoke("offlineMessageSender.sendToOffline");
+                            offlineMessageSender.sendToOffline(eventObj);
+                        } catch (Exception ex) {
+                            fault("offlineMessageSender.sendToOffline");
+                            logger.error(Contexts.getLogPrefix() + "send Offline4J message fault.", ex);
+                        } finally {
+                            afterInvoke("offlineMessageSender.sendToOffline");
+                        }
                     }
                 }
 
