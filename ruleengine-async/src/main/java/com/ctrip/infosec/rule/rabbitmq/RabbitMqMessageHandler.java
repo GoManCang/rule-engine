a8aab2c262c6863f1cfbd87aa8beed4d7ec8ab4e
vjyx蒋一新 <yxjiang@ctrip.com>
2015-07-16 10:21:53 +0800

    重构落地代码到独立的类

--- ruleengine-async/src/main/java/com/ctrip/infosec/rule/rabbitmq/RabbitMqMessageHandler.java
+++ ruleengine-async/src/main/java/com/ctrip/infosec/rule/rabbitmq/RabbitMqMessageHandler.java
@@ -10,7 +10,6 @@ import static com.ctrip.infosec.common.SarsMonitorWrapper.beforeInvoke;
 import static com.ctrip.infosec.common.SarsMonitorWrapper.fault;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -18,12 +17,8 @@ import java.util.Map.Entry;
 import com.ctrip.infosec.configs.event.*;
 import com.ctrip.infosec.configs.event.enums.PersistColumnSourceType;
 import com.ctrip.infosec.rule.convert.RiskFactConvertRuleService;
-import com.ctrip.infosec.rule.convert.RiskFactPersistStrategy;
 import com.ctrip.infosec.rule.convert.persist.*;
 import com.ctrip.infosec.rule.executor.*;
-import com.ctrip.infosec.rule.resource.RiskLevelData;
-import com.ctrip.infosec.rule.resource.model.SaveRiskLevelDataRequest;
-import com.ctrip.infosec.rule.resource.model.SaveRiskLevelDataResponse;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.apache.commons.beanutils.PropertyUtils;
@@ -61,8 +56,6 @@ public class RabbitMqMessageHandler {
     @Autowired
     private PostRulesExecutorService postRulesExecutorService;
     @Autowired
-    private PersistPreRuleExecutorService persistPreRuleExecutorService;
-    @Autowired
     private DispatcherMessageSender dispatcherMessageSender;
     @Autowired
     private CallbackMessageSender callbackMessageSender;
@@ -74,14 +67,12 @@ public class RabbitMqMessageHandler {
     private CounterPushRulesExecutorService counterPushRuleExrcutorService;
     @Autowired
     private RiskEventConvertor riskEventConvertor;
-
     @Autowired
-    private RiskFactConvertRuleService riskFactConvertRuleService;
+    private Offline4jService offline4jService;
 
     public void handleMessage(Object message) throws Exception {
         RiskFact fact = null;
-        String factTxt = null;
-        long reqId;
+        String factTxt;
         InternalRiskFact internalRiskFact = null;
         try {
 
@@ -152,71 +143,7 @@ public class RabbitMqMessageHandler {
             }
             // -------------------------------- 规则引擎结束 -------------------------------------- //
 
-            // 执行落地前规则
-            persistPreRuleExecutorService.executePostRules(fact, false);
-            //riskfact 数据映射转换
-            internalRiskFact = riskFactConvertRuleService.apply(fact);
-            if (internalRiskFact != null) {
-                // 数据落地
-                String operation = internalRiskFact.getEventPoint() + ".persist-info";
-                try {
-                    beforeInvoke(operation);
-                    Integer riskLevel = MapUtils.getInteger(fact.finalResult, Constants.riskLevel, 0);
-                    String resultRemark = "NEW: " + resultToString(fact.results);
-                    RiskFactPersistManager persistManager = RiskFactPersistStrategy.preparePersistence(internalRiskFact);
-                    PersistContext persistContext = persistManager.persist(riskLevel, resultRemark);
-                    reqId = persistManager.getGeneratedReqId();
-                    internalRiskFact.setReqId(reqId);
-                    // 调用远程服务落地
-                    if (MapUtils.getBoolean(fact.ext, "offline4j-push-ebank", false)) {
-                        SaveRiskLevelDataRequest request = new SaveRiskLevelDataRequest();
-                        request.setResID(reqId);
-                        request.setReqID(reqId);
-                        request.setOrderID(persistManager.getLong("InfoSecurity_RiskLevelData.OrderID"));
-                        request.setRiskLevel(riskLevel);
-                        request.setRemark(persistManager.getString("InfoSecurity_RiskLevelData.Remark"));
-                        request.setOrderType(persistManager.getInteger("InfoSecurity_RiskLevelData.OrderID"));
-                        request.setOriginalRiskLevel(riskLevel);
-                        Map<String, Object> ebankData = MapUtils.getMap(fact.ext, "ebank-data");
-                        request.setInfoID(MapUtils.getInteger(ebankData, "infoId", 0));
-                        request.setIsForigenCard(MapUtils.getString(ebankData, "isForeignCard", ""));
-                        request.setCardInfoID(MapUtils.getInteger(ebankData, "cardInfoID", 0));
-
-                        SaveRiskLevelDataResponse ebankResp = RiskLevelData.save(request);
-                        if (ebankResp != null) {
-                            // 更新InfoSecurity_RiskLevelData的TransFlag = 32
-                            RdbmsUpdate update = new RdbmsUpdate();
-                            DistributionChannel channel = new DistributionChannel();
-                            String allInOneDb = RiskFactPersistStrategy.allInOne4ReqId;
-                            channel.setChannelNo(allInOneDb);
-                            channel.setDatabaseType(DatabaseType.AllInOne_SqlServer);
-                            channel.setChannelDesc(allInOneDb);
-                            channel.setDatabaseURL(allInOneDb);
-                            update.setChannel(channel);
-
-                            Map<String, PersistColumnProperties> map = new HashMap<>();
-                            PersistColumnProperties pcp = new PersistColumnProperties();
-                            pcp.setValue(reqId);
-                            pcp.setColumnType(DataUnitColumnType.Long);
-                            pcp.setPersistColumnSourceType(PersistColumnSourceType.DB_PK);
-                            map.put("ReqID", pcp);
-
-                            pcp = new PersistColumnProperties();
-                            pcp.setValue(32);
-                            pcp.setPersistColumnSourceType(PersistColumnSourceType.DATA_UNIT);
-                            pcp.setColumnType(DataUnitColumnType.Int);
-                            map.put("TransFlag", pcp);
-
-                            update.execute(persistContext);
-                        }
-                    }
-                } catch (Exception ex) {
-                    fault(operation);
-                    logger.error(Contexts.getLogPrefix() + "fail to persist risk fact.", ex);
-                } finally {
-                    afterInvoke(operation);
-                }
-            }
+            internalRiskFact = offline4jService.saveForOffline(fact);
 
             // 落地规则结果
             beforeInvoke("CardRiskDB.CheckResultLog.saveRuleResult");
@@ -419,29 +346,6 @@ public class RabbitMqMessageHandler {
                 }
             }
         }
-    }
-
-    private String resultToString(Map<String, Map<String, Object>> results) {
-        List<String> result = Lists.newArrayList();
-        if (MapUtils.isNotEmpty(results)) {
-            for (Entry<String, Map<String, Object>> entry : results.entrySet()) {
-                try {
-                    Map<String, Object> val = entry.getValue();
-                    if (val != null) {
-                        Object level = val.get("riskLevel");
-                        if (level != null) {
-                            int riskLevel = Integer.valueOf(level.toString());
-                            if (riskLevel > 0) {
-                                result.add(entry.getKey());
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    logger.error(Contexts.getLogPrefix() + "get risk level from results failed.", e);
-                }
-            }
-        }
-        return StringUtils.join(result, ',');
     }
 
     /**
---
+++ ruleengine-core/src/main/java/com/ctrip/infosec/rule/executor/Offline4jService.java
@@ -0,0 +1,138 @@
+package com.ctrip.infosec.rule.executor;
+
+import com.ctrip.infosec.common.Constants;
+import com.ctrip.infosec.common.model.RiskFact;
+import com.ctrip.infosec.configs.event.DataUnitColumnType;
+import com.ctrip.infosec.configs.event.DatabaseType;
+import com.ctrip.infosec.configs.event.DistributionChannel;
+import com.ctrip.infosec.configs.event.enums.PersistColumnSourceType;
+import com.ctrip.infosec.rule.Contexts;
+import com.ctrip.infosec.rule.convert.RiskFactConvertRuleService;
+import com.ctrip.infosec.rule.convert.RiskFactPersistStrategy;
+import com.ctrip.infosec.rule.convert.internal.InternalRiskFact;
+import com.ctrip.infosec.rule.convert.persist.PersistColumnProperties;
+import com.ctrip.infosec.rule.convert.persist.PersistContext;
+import com.ctrip.infosec.rule.convert.persist.RdbmsUpdate;
+import com.ctrip.infosec.rule.convert.persist.RiskFactPersistManager;
+import com.ctrip.infosec.rule.resource.RiskLevelData;
+import com.ctrip.infosec.rule.resource.model.SaveRiskLevelDataRequest;
+import com.ctrip.infosec.rule.resource.model.SaveRiskLevelDataResponse;
+import com.google.common.collect.Lists;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.ctrip.infosec.common.SarsMonitorWrapper.afterInvoke;
+import static com.ctrip.infosec.common.SarsMonitorWrapper.beforeInvoke;
+import static com.ctrip.infosec.common.SarsMonitorWrapper.fault;
+
+/**
+ * Created by yxjiang on 2015/7/16.
+ */
+@Service
+public class Offline4jService {
+    private static final Logger logger = LoggerFactory.getLogger(Offline4jService.class);
+    @Autowired
+    private PersistPreRuleExecutorService persistPreRuleExecutorService;
+    @Autowired
+    private RiskFactConvertRuleService riskFactConvertRuleService;
+
+    public InternalRiskFact saveForOffline(RiskFact fact) {
+        long reqId;// 执行落地前规则
+        persistPreRuleExecutorService.executePostRules(fact, false);
+        //riskfact 数据映射转换
+        InternalRiskFact internalRiskFact = riskFactConvertRuleService.apply(fact);
+        if (internalRiskFact != null) {
+            // 数据落地
+            String operation = internalRiskFact.getEventPoint() + ".persist-info";
+            try {
+                beforeInvoke(operation);
+                Integer riskLevel = MapUtils.getInteger(fact.finalResult, Constants.riskLevel, 0);
+                String resultRemark = "NEW: " + resultToString(fact.results);
+                RiskFactPersistManager persistManager = RiskFactPersistStrategy.preparePersistence(internalRiskFact);
+                PersistContext persistContext = persistManager.persist(riskLevel, resultRemark);
+                reqId = persistManager.getGeneratedReqId();
+                internalRiskFact.setReqId(reqId);
+                // 调用远程服务落地
+                if (MapUtils.getBoolean(fact.ext, "offline4j-push-ebank", false)) {
+                    SaveRiskLevelDataRequest request = new SaveRiskLevelDataRequest();
+                    request.setResID(reqId);
+                    request.setReqID(reqId);
+                    request.setOrderID(persistManager.getLong("InfoSecurity_RiskLevelData.OrderID"));
+                    request.setRiskLevel(riskLevel);
+                    request.setRemark(persistManager.getString("InfoSecurity_RiskLevelData.Remark"));
+                    request.setOrderType(persistManager.getInteger("InfoSecurity_RiskLevelData.OrderID"));
+                    request.setOriginalRiskLevel(riskLevel);
+                    Map<String, Object> ebankData = MapUtils.getMap(fact.ext, "ebank-data");
+                    request.setInfoID(MapUtils.getInteger(ebankData, "infoId", 0));
+                    request.setIsForigenCard(MapUtils.getString(ebankData, "isForeignCard", ""));
+                    request.setCardInfoID(MapUtils.getInteger(ebankData, "cardInfoID", 0));
+
+                    SaveRiskLevelDataResponse ebankResp = RiskLevelData.save(request);
+                    if (ebankResp != null) {
+                        // 更新InfoSecurity_RiskLevelData的TransFlag = 32
+                        RdbmsUpdate update = new RdbmsUpdate();
+                        DistributionChannel channel = new DistributionChannel();
+                        String allInOneDb = RiskFactPersistStrategy.allInOne4ReqId;
+                        channel.setChannelNo(allInOneDb);
+                        channel.setDatabaseType(DatabaseType.AllInOne_SqlServer);
+                        channel.setChannelDesc(allInOneDb);
+                        channel.setDatabaseURL(allInOneDb);
+                        update.setChannel(channel);
+
+                        Map<String, PersistColumnProperties> map = new HashMap<>();
+                        PersistColumnProperties pcp = new PersistColumnProperties();
+                        pcp.setValue(reqId);
+                        pcp.setColumnType(DataUnitColumnType.Long);
+                        pcp.setPersistColumnSourceType(PersistColumnSourceType.DB_PK);
+                        map.put("ReqID", pcp);
+
+                        pcp = new PersistColumnProperties();
+                        pcp.setValue(32);
+                        pcp.setPersistColumnSourceType(PersistColumnSourceType.DATA_UNIT);
+                        pcp.setColumnType(DataUnitColumnType.Int);
+                        map.put("TransFlag", pcp);
+
+                        update.execute(persistContext);
+                    }
+                }
+            } catch (Exception ex) {
+                fault(operation);
+                logger.error(Contexts.getLogPrefix() + "fail to persist risk fact.", ex);
+            } finally {
+                afterInvoke(operation);
+            }
+        }
+        return internalRiskFact;
+    }
+
+    private String resultToString(Map<String, Map<String, Object>> results) {
+        List<String> result = Lists.newArrayList();
+        if (MapUtils.isNotEmpty(results)) {
+            for (Map.Entry<String, Map<String, Object>> entry : results.entrySet()) {
+                try {
+                    Map<String, Object> val = entry.getValue();
+                    if (val != null) {
+                        Object level = val.get("riskLevel");
+                        if (level != null) {
+                            int riskLevel = Integer.valueOf(level.toString());
+                            if (riskLevel > 0) {
+                                result.add(entry.getKey());
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    logger.error(Contexts.getLogPrefix() + "get risk level from results failed.", e);
+                }
+            }
+        }
+        return StringUtils.join(result, ',');
+    }
+}
