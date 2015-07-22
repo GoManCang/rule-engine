a681e9a133a3463e3c97f463331b1be0dc673aa5
vjyx蒋一新 <yxjiang@ctrip.com>
2015-07-22 13:35:43 +0800

    修改bug：级联的metadata未获取到值

--- ruleengine-convert/src/main/java/com/ctrip/infosec/rule/convert/config/ConvertRuleUpdateCallback.java
+++ ruleengine-convert/src/main/java/com/ctrip/infosec/rule/convert/config/ConvertRuleUpdateCallback.java
@@ -2,7 +2,15 @@ package com.ctrip.infosec.rule.convert.config;
 
 import com.ctrip.infosec.configs.Caches;
 import com.ctrip.infosec.configs.ConfigsLoadedCallback;
+import com.ctrip.infosec.configs.event.DataUnitColumn;
+import com.ctrip.infosec.configs.event.DataUnitMetadata;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.jmx.export.annotation.ManagedResource;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yxjiang on 2015/6/15.
@@ -15,7 +23,23 @@ public class ConvertRuleUpdateCallback implements ConfigsLoadedCallback {
 //        ConverterLocator.updateInteralConvertConfig();
         InternalConvertConfigHolder.reconfigure(Caches.riskFactConvertRuleConfigs,
                 Caches.internalRiskFactDefinitionConfigs
-                );
+        );
         RiskFactPersistConfigHolder.reconfigure(Caches.internalRiskFactPersistConfigs, Caches.dataUnitMetadatas);
+        initDataUnitMatadata(Caches.dataUnitMetadatas);
+    }
+
+    private void initDataUnitMatadata(Map<String, DataUnitMetadata> dataUnitMetadatas) {
+        if (MapUtils.isNotEmpty(dataUnitMetadatas)) {
+            for (DataUnitMetadata metadata : dataUnitMetadatas.values()) {
+                List<DataUnitColumn> columns = metadata.getColumns();
+                if (CollectionUtils.isNotEmpty(columns)) {
+                    for (DataUnitColumn column : columns) {
+                        if(StringUtils.isNotBlank(column.getNestedDataUnitMataNo())){
+                            column.setNestedDataUnitMeta(dataUnitMetadatas.get(column.getNestedDataUnitMataNo()));
+                        }
+                    }
+                }
+            }
+        }
     }
 }
