1f94bf1e2656a189ff4977148dc867dd43c64f2f
zhengby <zhengby@DST61666.cn1.global.ctrip.com>
2015-07-23 15:38:28 +0800

    优化TraceLogger埋点

--- ruleengine-bundle/src/main/java/com/ctrip/infosec/rule/util/Emitter.java
+++ ruleengine-bundle/src/main/java/com/ctrip/infosec/rule/util/Emitter.java
@@ -111,12 +111,22 @@ public class Emitter {
 
                         boolean withScene = Constants.eventPointsWithScene.contains(fact.eventPoint);
                         TraceLogger.traceLog("[trace] withScene = " + withScene + ", scenes = [" + (scenes == null ? "" : scenes) + "]");
-                        if (!withScene && StringUtils.isNotBlank(scenes)) {
-                            TraceLogger.traceLog("&gt;&gt;&gt;&gt; [" + ruleNo + "] 执行结果: [在非适配点指定了场景、忽略此次结果] riskLevel = " + riskLevel
-                                    + ", riskMessage = " + riskMessage + ", riskScene = [" + scenes + "]");
-                        } else if (withScene && StringUtils.isBlank(scenes)) {
-                            TraceLogger.traceLog("&gt;&gt;&gt;&gt; [" + ruleNo + "] 执行结果: [没有指定场景、忽略此次结果] riskLevel = " + riskLevel
-                                    + ", riskMessage = " + riskMessage);
+                        if (!withScene) {
+                            if (StringUtils.isNotBlank(scenes)) {
+                                TraceLogger.traceLog("&gt;&gt;&gt;&gt; [" + ruleNo + "] 执行结果: [在非适配点指定了场景、忽略此次结果] riskLevel = " + riskLevel
+                                        + ", riskMessage = " + riskMessage + ", riskScene = [" + scenes + "]");
+                            } else {
+                                TraceLogger.traceLog("&gt;&gt;&gt;&gt; [" + ruleNo + "] 执行结果: riskLevel = " + riskLevel
+                                        + ", riskMessage = " + riskMessage);
+                            }
+                        } else if (withScene) {
+                            if (StringUtils.isBlank(scenes)) {
+                                TraceLogger.traceLog("&gt;&gt;&gt;&gt; [" + ruleNo + "] 执行结果[适配]: [没有指定场景、忽略此次结果] riskLevel = " + riskLevel
+                                        + ", riskMessage = " + riskMessage);
+                            } else {
+                                TraceLogger.traceLog("&gt;&gt;&gt;&gt; [" + ruleNo + "] 执行结果[适配]: riskLevel = " + riskLevel
+                                        + ", riskMessage = " + riskMessage + ", riskScene = [" + scenes + "]");
+                            }
                         }
                     }
                 }
