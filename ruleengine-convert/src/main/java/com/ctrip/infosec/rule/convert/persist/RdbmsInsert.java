30c470dfe44c3eb9b38378127ba45290ff4b43f9
vjyx蒋一新 <yxjiang@ctrip.com>
2015-08-11 20:30:47 +0800

    增加调试信息

--- ruleengine-convert/src/main/java/com/ctrip/infosec/rule/convert/persist/RdbmsInsert.java
+++ ruleengine-convert/src/main/java/com/ctrip/infosec/rule/convert/persist/RdbmsInsert.java
@@ -59,7 +59,7 @@ public class RdbmsInsert extends AbstractRdbmsOperation {
 
             } catch (Exception e) {
                 try {
-                    logger.warn("schema={}, user={}", connection.getSchema(), connection.getMetaData().getUserName());
+                    logger.warn("user={}", connection.getMetaData().getUserName());
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
