ba273553c6c8117d8a34b1c2be19863446886ec7
unknown <lpxie@DST58695.cn1.global.ctrip.com>
2015-09-06 12:03:28 +0800

    添加同盾调用服务

---
+++ ruleengine-bundle/src/test/java/com/ctrip/infosec/rule/resource/TongDunServiceTest.java
@@ -0,0 +1,18 @@
+package com.ctrip.infosec.rule.resource;
+
+import org.junit.Test;
+
+/**
+ * Created by lpxie on 15-9-6.
+ */
+public class TongDunServiceTest {
+    @Test
+    public void test()
+    {
+        String ip = "8.8.8.8";
+        String mobile = "18740223918";
+        String score = TongDunService.queryT(ip,mobile);
+        String score1 = TongDunService.queryR(ip,mobile);
+        System.out.println(score+"------"+score1);
+    }
+}
