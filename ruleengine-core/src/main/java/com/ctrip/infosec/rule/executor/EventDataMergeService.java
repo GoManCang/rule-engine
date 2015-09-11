d0bf07ca7200a8b3643c807d08bbb903ec9c8569
zhengby <zhengby@DST61666.cn1.global.ctrip.com>
2015-09-11 17:19:50 +0800

    优化redis合并逻辑

--- ruleengine-core/src/main/java/com/ctrip/infosec/rule/executor/EventDataMergeService.java
+++ ruleengine-core/src/main/java/com/ctrip/infosec/rule/executor/EventDataMergeService.java
@@ -111,15 +111,26 @@ public class EventDataMergeService {
             for (String sourceFieldName : fieldMapping.keySet()) {
                 String targetFieldName = fieldMapping.get(sourceFieldName);
                 Object targetFieldValue = valueMap.get(sourceFieldName);
-                if (targetFieldName == null || targetFieldName.isEmpty() || targetFieldValue == null) {
+                if (StringUtils.isEmpty(targetFieldName)
+                        || targetFieldValue == null
+                        || (targetFieldValue instanceof String && StringUtils.isEmpty((String) targetFieldValue))) {
                     continue;
                 }
-                if (targetFieldValue instanceof Map || targetFieldValue instanceof List || targetFieldValue instanceof Object[]) {
-                    TraceLogger.traceLog("GET: " + sourceFieldName + " &DoubleRightArrow; " + targetFieldName + ", value = " + JSON.toJSONString(targetFieldValue));
-                } else {
-                    TraceLogger.traceLog("GET: " + sourceFieldName + " &DoubleRightArrow; " + targetFieldName + ", value = " + targetFieldValue);
+                // eventBody对应的KEY有值的话 不覆盖
+                Object fv = fact.eventBody.get(targetFieldName);
+                if (fv == null
+                        || (fv instanceof String && StringUtils.isEmpty((String) fv))
+                        || (fv instanceof Map && ((Map) fv).isEmpty())
+                        || (fv instanceof List && ((List) fv).isEmpty())
+                        || (fv instanceof Object[] && ((Object[]) fv).length == 0)) {
+
+                    if (targetFieldValue instanceof Map || targetFieldValue instanceof List || targetFieldValue instanceof Object[]) {
+                        TraceLogger.traceLog("GET: " + sourceFieldName + " &DoubleRightArrow; " + targetFieldName + ", value = " + JSON.toJSONString(targetFieldValue));
+                    } else {
+                        TraceLogger.traceLog("GET: " + sourceFieldName + " &DoubleRightArrow; " + targetFieldName + ", value = " + targetFieldValue);
+                    }
+                    fact.eventBody.put(targetFieldName, targetFieldValue);
                 }
-                fact.eventBody.put(targetFieldName, targetFieldValue);
             }
         }
         return fact;
@@ -150,16 +161,20 @@ public class EventDataMergeService {
             }
             Set<String> fields = fieldsToPut.get(key);
             for (String fieldName : fields) {
-                Object fieldValue = fact.eventBody.get(fieldName);
-                if (fieldValue == null || (fieldValue instanceof String && StringUtils.isEmpty((String) fieldValue))) {
+                Object fv = fact.eventBody.get(fieldName);
+                if (fv == null
+                        || (fv instanceof String && StringUtils.isEmpty((String) fv))
+                        || (fv instanceof Map && ((Map) fv).isEmpty())
+                        || (fv instanceof List && ((List) fv).isEmpty())
+                        || (fv instanceof Object[] && ((Object[]) fv).length == 0)) {
                     continue;
                 }
-                if (fieldValue instanceof Map || fieldValue instanceof List || fieldValue instanceof Object[]) {
-                    TraceLogger.traceLog("PUT: " + fieldName + " = " + JSON.toJSONString(fieldValue));
+                if (fv instanceof Map || fv instanceof List || fv instanceof Object[]) {
+                    TraceLogger.traceLog("PUT: " + fieldName + " = " + JSON.toJSONString(fv));
                 } else {
-                    TraceLogger.traceLog("PUT: " + fieldName + " = " + fieldValue);
+                    TraceLogger.traceLog("PUT: " + fieldName + " = " + fv);
                 }
-                valueMap.put(fieldName, fieldValue);
+                valueMap.put(fieldName, fv);
             }
 
             String value = JSON.toJSONString(valueMap);
