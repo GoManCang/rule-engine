a66be413e9e8a0d9f42dfc99a31ddbdd2f31e849
zhengby <zhengby@DST61666.cn1.global.ctrip.com>
2015-09-23 18:57:27 +0800

    优化合并处理

--- ruleengine-ws/src/main/java/com/ctrip/infosec/rule/rest/RuleEngineRESTfulController.java
+++ ruleengine-ws/src/main/java/com/ctrip/infosec/rule/rest/RuleEngineRESTfulController.java
@@ -82,6 +82,16 @@ public class RuleEngineRESTfulController {
                 TraceLogger.commitTrans();
                 RuleMonitorHelper.commitTrans(fact);
             }
+            // 执行数据合并（PUT）
+            try {
+                RuleMonitorHelper.newTrans(fact, RuleMonitorType.PUT);
+                TraceLogger.beginTrans(fact.eventId, "S1");
+                TraceLogger.setLogPrefix("[同步数据合并]");
+                eventDataMergeService.executeRedisPut(fact);
+            } finally {
+                TraceLogger.commitTrans();
+                RuleMonitorHelper.commitTrans(fact);
+            }
             // 执行预处理            
             try {
                 RuleMonitorHelper.newTrans(fact, RuleMonitorType.PRE_RULE_WRAP);
